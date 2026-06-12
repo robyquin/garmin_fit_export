6b4474476302840038fd94ff400ed081cef37bbf
robyquin <robyquin@gmail.com>
2026-06-12 08:11:38 +0200

    Release version v.0.2.0 (+ svg)

--- garmin_fit_export/garmin_fit_export.py
+++ garmin_fit_export/garmin_fit_export.py
@@ -23,7 +23,7 @@ logging.basicConfig(format='%(asctime)s - [%(levelname)s] %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
 
 __name__ = "GarminFitExport"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 
 class GarminFitExport():
